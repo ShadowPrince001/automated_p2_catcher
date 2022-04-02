3d48b247f3c71df313bc8f59503de448fe34b9b5
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-02 21:36:03 +0530

    Add files via upload

--- p2.py
+++ p2.py
@@ -1,29 +1,19 @@
-import requests
-import json
-import time
+from pokemon_list import pokemon_list
 
-
-channel_id = "956528701175124041"
-
-
-def retrieve_messages(channel_id):
-    headers = {
-        "authorization": "mfa.yZiDpjSp50M0Uv_B4zBOtkreV8ZU6mTscky4y9r7X-3N58e1t1bPAGAud5CsBtMD6SltqkrsVwPQv67BlVBr"
-    }
-    r = requests.get(
-        f"https://discord.com/api/v9/channels/{channel_id}/messages", headers=headers
-    )
-    jsonn = json.loads(r.text)
-    for value in jsonn:
-        content = value["content"]
-        username = value["author"]["username"]
-        attachment = value["attachments"]
-        if "The pokémon is" in content:
-            print("Hint")
-        elif bool(attachment):
-            print("Pokemon")
-
-
-while True:
-    retrieve_messages(channel_id)
-    time.sleep(10)
+name = "M__"
+length = len(name)
+count = 0
+for a in name:
+    if (a.isalpha()) == True:
+        count += 1
+counter = 0
+for a in pokemon_list:
+    if length == len(a):
+        for r in range(length):
+            if name[r] is a[r]:
+                counter += 1
+        if counter == count:
+            print(name, "is", a)
+            counter = 0
+        else:
+            counter = 0
