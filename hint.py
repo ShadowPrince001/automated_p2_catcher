4aca42331d48eb6a79bdcb46f7f89237360295d3
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-04 23:22:29 +0530

    Add files via upload

---
+++ hint.py
@@ -0,0 +1,14 @@
+import requests
+
+
+def hint_message():
+
+    payload = {"content": "p!hint"}
+    header = {
+        "authorization": "mfa.yZiDpjSp50M0Uv_B4zBOtkreV8ZU6mTscky4y9r7X-3N58e1t1bPAGAud5CsBtMD6SltqkrsVwPQv67BlVBr"
+    }
+    r = requests.post(
+        "https://discord.com/api/v9/channels/956528701175124041/messages",
+        data=payload,
+        headers=header,
+    )
