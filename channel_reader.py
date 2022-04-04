f6adfc2c19e4ccd3675af3b92343d24729a130bd
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-04 23:19:08 +0530

    Add files via upload

--- channel_reader.py
+++ channel_reader.py
@@ -1,7 +1,10 @@
 import requests
 import json
 import time
-
+from text_sender import send_message
+from clear import clear_message
+from pokemon_list import pokemon_list
+from hint import hint_message
 
 channel_id = "956528701175124041"
 
@@ -16,14 +19,43 @@ def retrieve_messages(channel_id):
     jsonn = json.loads(r.text)
     for value in jsonn:
         content = value["content"]
-        username = value["author"]["username"]
+        usercontent = value["author"]["username"]
         attachment = value["attachments"]
-        if "The pokmon is" in content:
-            print("Hint")
-        elif bool(attachment):
-            print("Pokemon")
+        embed = value["embeds"]
+        if "The pokémon is" in content:
+            print(content)
+            slc = slice(15, -1)
+            content = content[slc]
+            print(content)
+            content = content.replace("\\", "")
+            print(content)
+            length = len(content)
+            count = 0
+            for x in content:
+                if (x.isalpha()) == True:
+                    count += 1
+            counter = 0
+            for a in pokemon_list:
+                if length == len(a):
+                    for r in range(length):
+                        if content[r] is a[r]:
+                            counter += 1
+                    if counter == count:
+                        print(a)
+                        a = "p!c " + a
+                        payload = {"content": a}
+                        send_message(payload)
+                        counter = 0
+                        clear_message()
+                    else:
+                        counter = 0
+
+        elif bool(embed) or bool(attachment):
+            clear_message()
+
+            hint_message()
 
 
 while True:
-    retrieve_messages(channel_id)
+    retrieve_messages("956528701175124041")
     time.sleep(10)
