477771950c3f8dbc614a9c0167694ff6bd95800c
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-04 23:23:51 +0530

    Update hint.py

--- hint.py
+++ hint.py
@@ -5,10 +5,10 @@ def hint_message():
 
     payload = {"content": "p!hint"}
     header = {
-        "authorization": "mfa.yZiDpjSp50M0Uv_B4zBOtkreV8ZU6mTscky4y9r7X-3N58e1t1bPAGAud5CsBtMD6SltqkrsVwPQv67BlVBr"
+        "authorization": ""
     }
     r = requests.post(
-        "https://discord.com/api/v9/channels/956528701175124041/messages",
+        "",
         data=payload,
         headers=header,
     )
