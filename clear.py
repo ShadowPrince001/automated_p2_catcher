94e7253c673e31215c19752dd3e2fbcb6c19740a
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-04 23:23:19 +0530

    Update clear.py

--- clear.py
+++ clear.py
@@ -5,10 +5,10 @@ def clear_message():
 
     payload = {"content": "?purge 10"}
     header = {
-        "authorization": "mfa.yZiDpjSp50M0Uv_B4zBOtkreV8ZU6mTscky4y9r7X-3N58e1t1bPAGAud5CsBtMD6SltqkrsVwPQv67BlVBr"
+        "authorization": ""
     }
     r = requests.post(
-        "https://discord.com/api/v9/channels/956528701175124041/messages",
+        "",
         data=payload,
         headers=header,
     )
