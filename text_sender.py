b179b6b671bf89831d45fb5d0b92247ea9a27fd7
ShadowPrince001 <89309926+ShadowPrince001@users.noreply.github.com>
2022-04-10 21:32:59 +0530

    Add files via upload

--- text_sender.py
+++ text_sender.py
@@ -1,13 +1,40 @@
+import discord
 import requests
+import helper
+
+config = helper.read_config()
+
+channel_id = config["Pokebot"]["channel_id"]
+
+authorization = config["Pokebot"]["authorization"]
 
 
 def send_message(payload):
-
-    header = {
-        "authorization": ""
-    }
+    headers = {"authorization": authorization}
     r = requests.post(
-        "",
+        f"https://discord.com/api/v9/channels/{channel_id}/messages",
         data=payload,
-        headers=header,
+        headers=headers,
+    )
+
+
+def clear_message():
+
+    payload = {"content": "?purge 50"}
+    headers = {"authorization": authorization}
+    r = requests.post(
+        f"https://discord.com/api/v9/channels/{channel_id}/messages",
+        data=payload,
+        headers=headers,
+    )
+
+
+def hint_message():
+
+    payload = {"content": "p!hint"}
+    headers = {"authorization": authorization}
+    r = requests.post(
+        f"https://discord.com/api/v9/channels/{channel_id}/messages",
+        data=payload,
+        headers=headers,
     )
